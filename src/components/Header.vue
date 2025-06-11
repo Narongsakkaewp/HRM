611177e481bb513d4fd6d3cfb29b927a027e435e
Narongsakkaewp <152575175+Narongsakkaewp@users.noreply.github.com>
2025-06-11 17:59:56 +0700

    Edit LeftNavbar.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
-  <header class="bg-gray-800 text-white p-4 flex justify-between items-center">
+  <header class="bg-gray-700 text-white p-4 flex justify-between items-center">
     <h1 class="text-lg font-bold">VBEYOND</h1>
-    <button class="bg-red-600 hover:bg-red-700 p-2 rounded">
-      <i class="fas fa-power-off"></i>
+    <button class="bg-red-600 hover:bg-red-700 p-4 rounded">
+      <i class="fa-solid fa-right-from-bracket"></i>
     </button>
   </header>
 </template>
